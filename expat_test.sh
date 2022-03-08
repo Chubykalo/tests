15919dc2b3a7b72ec92de5507b28d3f4756699e1
Chubykalo <63315325+Chubykalo@users.noreply.github.com>
2022-03-08 13:01:30 +0100

    Create expat_test.sh

---
+++ expat_test.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+mkdir expat_test && cd $_
+
+cat << EOF > example.xml
+<?xml version="1.0" encoding="UTF-8"?>
+<note>
+  <to>Tove</to>
+  <from>Jani</from>
+  <heading>Reminder</heading>
+  <body>Don't forget me this weekend!</body>
+</note>
+EOF
+
+cat << EOF > example-error.xml
+<?xml version="1.0" encoding="UTF-8"?>
+<note>
+  <to>Tove</to>
+  <from>Jani</Ffrom>
+  <heading>Reminder</heading>
+  <body>Don't forget me this weekend!</body>
+</note>
+EOF
+
+xmlwf -- example.xml
+xmlwf -- example-error.xml
